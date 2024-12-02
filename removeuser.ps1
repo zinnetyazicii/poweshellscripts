aa0cbb5380a50fd188828b2d1a476303b15b5c8a
emrahtolu <emrahtolu@hotmail.com.tr>
2024-12-02 14:25:26 +0300

    Add files via upload
    örnek csv içeriği 
    sAMAccountName
    emrah.tolu

---
+++ removeuser.ps1
@@ -0,0 +1,5 @@
+﻿$users = Import-Csv -Path C:\scripts\removeuser.csv
+
+foreach ($user in $users) {
+    Remove-ADObject -Identity (Get-ADUser $user.SamAccountName).DistinguishedName -Recursive -Confirm:$false
+}
